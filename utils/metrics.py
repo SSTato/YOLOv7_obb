dfb0ad7c995679e642636f6d8d8a2077bff9ca46
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2023-01-29 17:36:45 +0800

    Mean angular method
    Mean angular method + KFIOU prep revised

--- utils/metrics.py
+++ utils/metrics.py
@@ -246,18 +246,40 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, KFIO
             xywhbox2 = box2
         newbox1 = torch.zeros_like(box1).to(device)
         sze = newbox1.size(dim=1)
-        newbox1 = torch.cat((newbox1, torch.zeros(sze)), 0).to(device)
+        catcon1 = torch.zeros(1, sze).to(device)
+        newbox1 = torch.cat((newbox1, catcon1), 0).to(device)
         newbox1[:4, :] = xywhbox1[:4, :]
-        newbox1[4] = ptheta
+        pthetanew = torch.zeros(sze).to(device)
+        for i in range(sze):
+            pthetanew[i] = ptheta[i, :].mean() #we have learned that there are 180 angular dimensions for each box(895), calculating their mean will achieve an average angle of each box in radians
+        newbox1[4, :] = pthetanew
+
+        #newbox1a = torch.zeros(sze, 5).to(device)
+        newbox1a = torch.transpose(newbox1, 0, 1).to(device)
+        #for i in range(sze):
+            #for j in range(5):
+                #newbox1a[i, j] = newbox1[j, i]
         
         newbox2 = torch.zeros_like(box2).to(device)
         sze = newbox2.size(dim=1)
-        newbox2 = torch.cat((newbox2, torch.zeros(sze)), 0).to(device)
-        newbox2[:4,:] = xywhbox2[:,:4]
-        newbox2[4] = ttheta
+        catcon2 = torch.zeros(1, sze).to(device)
+        newbox2 = torch.cat((newbox2, catcon2), 0).to(device)
+        newbox2[:4, :] = xywhbox2[:4, :]
+        tthetanew = torch.zeros(sze).to(device)
+        for i in range(sze):
+            tthetanew[i] = ttheta[i, :].mean()
+        newbox2[4, :] = tthetanew
+
+        #newbox2a = torch.zeros(sze, 5).to(device)
+        newbox2a = torch.transpose(newbox2, 0, 1).to(device)
+        #for i in range(sze):
+            #for j in range(5):
+                #newbox2a[i, j] = newbox2[j, i] 
         
-        kfbox1, sigma1 = kfiou.xy_wh_r_2_xy_sigma(newbox1)
-        kfbox2, sigma2 = kfiou.xy_wh_r_2_xy_sigma(newbox2)
+        #the indexing method in KFIOU is vastly differnt from other IOUs. Thus, a transpose is required
+        
+        kfbox1, sigma1 = kfiou.xy_wh_r_2_xy_sigma(newbox1a)
+        kfbox2, sigma2 = kfiou.xy_wh_r_2_xy_sigma(newbox2a)
 
     # Intersection area
     inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
@@ -283,7 +305,7 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, KFIO
                         alpha = v / (v - iou + (1 + eps))
                     return iou - (rho2 / c2 + v * alpha)  # CIoU
                 else:
-                    iou, loss_kfiou = kfiou.kfiou_loss(pred=newbox1, target=newbox2, pred_decode=newbox1, targets_decode=newbox2)
+                    iou, loss_kfiou = kfiou.kfiou_loss(pred=newbox1a, target=newbox2a, pred_decode=newbox1a, targets_decode=newbox2a)
                     return iou, loss_kfiou
             else:
                 return iou - rho2 / c2  # DIoU
