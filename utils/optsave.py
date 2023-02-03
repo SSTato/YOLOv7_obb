03cda72a6b175e18c803dd53ef031b228c225f3f
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2023-02-03 13:11:02 +0800

    Create optsave.py

---
+++ utils/optsave.py
@@ -0,0 +1,17 @@
+import pickle
+
+def savevar(var):
+
+    # Open a file and use dump()
+    with open('savevars.pkl', 'wb') as file:
+        # A new file will be created
+        pickle.dump(var, file)
+
+def loadvar():
+
+    # Open the file in binary mode
+    with open('file.pkl', 'rb') as file:
+        # Call load method to deserialze
+        var = pickle.load(file)
+        print(var + 'mode selected')
+        return var
